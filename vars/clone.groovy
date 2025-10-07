e481165f16779aad092c82221aab57d892c152ab
AbdulWadood <71840338+wadood786@users.noreply.github.com>
2025-10-08 03:31:49 +0500

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,3 @@
-def call(String url, String branch){
-  echo "this is cloning the code"
-  git url: "${url}", branch: "${branch}"
-  echo "Code cloned successfully"
+def call(string url, string branch){
+    git url : "${url}" , branch: "${branch}"
 }
