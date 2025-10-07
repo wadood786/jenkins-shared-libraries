8064dc2e8cfca7bf3d1442b902d8d263b70f3dcd
AbdulWadood <71840338+wadood786@users.noreply.github.com>
2025-10-08 04:10:12 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
-def call(string ProjectName, string ImageTag, string dockeHubUser){
-  withCredentials([usernamePassword('credentialsId':"dockerhubcred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+def call(string ProjectName, string ImageTag, string dockerHubUser){
+  withCredentials([usernamePassword(credentialsId:"dockerhubcred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
                 sh "docker login -u ${dockerHubUser} -p ${env.dockerHubPass}"
                 sh "docker push ${dockerHubUser}/${ProjectName}:${ImageTag}"
                 }
